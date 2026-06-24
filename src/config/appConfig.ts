bd1be426fc4861f5c6ef50e54b8f0dc01df0db39
Joao <joaofigura123@gmail.com>
2026-06-24 15:55:04 -0300

    Update appConfig.ts

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,6 +1,6 @@
 // Centralized app configuration.
 // Edit this single URL to update every "Baixar Aplicativo" button on the site.
-export const APP_DOWNLOAD_URL = "https://221carsbb1533.b-cdn.net/Cars2You.apk";
+export const APP_DOWNLOAD_URL = "https://241carssbb1533.b-cdn.net/Cars2You.apk";
 
 export const COMPANY = {
   name: "Cars2You",
